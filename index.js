1d1ca6d1519860e045d322a9d30be913f742d8c7
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-24 14:50:02 +0300

    Done

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   const reverseWord = reverseString(word);
+//   if( word === reverseWord) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+
+// function reverseString(word) {
+//   return word.split(' ').reverse().join(' ');
+// }
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const words = word.length
+  for (let i = 0; i < words /2; i ++ ){
+    if(word[i] !== word[words -1 -i]){
+      return false;
+    }
+  }
+  return true;
 }
+console.log("abba");
+
+
 
 /* 
   Add your pseudocode here
@@ -8,6 +34,7 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  Strict equlity operator is being used to return the reversed word thus solving the problem.
 */
 
 // You can run `node index.js` to view these console logs
